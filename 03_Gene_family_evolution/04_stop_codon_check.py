e183223021c8be29f3de2a791a05f77cc12c238e
LorenaDerezanin <LorenaDerezanin@users.noreply.github.com>
2022-02-20 18:01:23 +0100

    finish stop codon check, rm path

--- 03_Gene_family_evolution/04_stop_codon_check.py
+++ 03_Gene_family_evolution/04_stop_codon_check.py
@@ -1,4 +1,4 @@
-
+# Check for premature stop codons in nt sequences of paralogs
 import pandas as pd
 import os
 from textwrap import wrap
@@ -9,10 +9,15 @@ from Bio import SeqIO
 # squeeze = T - important for non-nested dict
 dna_codon_table = pd.read_csv('DNA_codon_table.csv', header=None, index_col=0, squeeze=True).to_dict()
 
-s = []
-path = '/run/user/1000/gvfs/sftp:host=62.141.164.6,port=22220,user=derezanin/home/derezanin/dorina_dir/BUSCO_gene_sets/gene_fam_expansions/expansions_all_sp/'
-for i in os.listdir(path):
-    for record in SeqIO.parse(path+i, "fasta"):
+stop_codons = []
+# path = # add path to dir ("BUSCO_gene_sets/gene_fam_expansions/expansions_all_sp")
+for file_name in os.listdir(path):
+    for record in SeqIO.parse(path + file_name, "fasta"):
         s = str(record.seq)
         dna_codons = wrap(s, 3)
-        
+        for p, c in enumerate(dna_codons):
+            if dna_codon_table.get(c) == "Stop" and p + 1 != len(dna_codons):
+                print(f"{file_name}:{record.id}: {p + 1}/{len(dna_codons)}, {c}")
+
+# output:
+# file name:record ID: codon position/number of codons, stop codon
