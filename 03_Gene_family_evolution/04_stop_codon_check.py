451782bb44737d528cda48e5a2131b8f13975c2b
LorenaDerezanin <LorenaDerezanin@users.noreply.github.com>
2022-02-21 09:56:02 +0100

    minor fix

--- 03_Gene_family_evolution/04_stop_codon_check.py
+++ 03_Gene_family_evolution/04_stop_codon_check.py
@@ -6,7 +6,7 @@ from Bio import SeqIO
 
 
 # create dictionary out of codon table
-# squeeze = T - important for non-nested dict
+# squeeze = T - for non-nested dict
 dna_codon_table = pd.read_csv('DNA_codon_table.csv', header=None, index_col=0, squeeze=True).to_dict()
 
 stop_codons = []
